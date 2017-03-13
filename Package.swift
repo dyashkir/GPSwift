112cd4d1f96277432dbb53ced2b22c897dc654f3
Dmytro Yashkir <dima.yashkir@gmail.com>
2017-03-13 15:30:30 +0900

    Surge

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "GPSwift"
+    name: "GPSwift",
+    dependencies: [
+        .Package(url: "https://github.com/dyashkir/Surge.git",
+                 majorVersion: 0)
+    ]
 )
